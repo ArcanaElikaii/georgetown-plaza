24840057b4f2f97131004b6ebd2d1c82f4d4cc39
ArcanaElikaii <arcanaelikaiii@gmail.com>
2025-04-28 11:04:32 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -4,3 +4,13 @@ function scrollUnits(direction) {
   const scrollAmount = 340;
   carousel.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
 }
+
+function showDetail(phase) {
+  const detail = document.getElementById('timeline-detail');
+  switch (phase) {
+    case 'design': detail.innerText = 'Design & Planning Phase'; break;
+    case 'demolition': detail.innerText = 'Demolition and Foundation Work'; break;
+    case 'interior': detail.innerText = 'Interior Construction'; break;
+    case 'finishing': detail.innerText = 'Finishing and Inspections'; break;
+  }
+}
